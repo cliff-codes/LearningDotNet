a141a43db6cb27ede4054f894c24ba97ef09d143
cliff-codes <simplecodes2580@gmail.com>
2025-06-25 23:15:42 +0000

    updates

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,3 +1,6 @@
+using System.Security.AccessControl;
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args); // kestrel server is created
 
 //we can configure our kestrel server here
@@ -5,28 +8,44 @@ var builder = WebApplication.CreateBuilder(args); // kestrel server is created
 var app = builder.Build(); // web application
 
 app.Run(async (HttpContext context) =>
+//working with http methods
 {
-    if (context.Request.Path.StartsWithSegments("/"))
+    if (context.Request.Method == "GET")
     {
-        await context.Response.WriteAsync($"This method is :  {context.Request.Method}\r\n");
-        await context.Response.WriteAsync($"This method is :  {context.Request.Path}\r\n");
         
-        foreach( var key in context.Request.Headers.Keys )
+        if (context.Request.Path.StartsWithSegments("/"))
         {
-            await context.Response.WriteAsync($"{key} : {context.Request.Headers[key]}\r\n");
+            await context.Response.WriteAsync($"This method is :  {context.Request.Method}\r\n");
+            await context.Response.WriteAsync($"This method is :  {context.Request.Path}\r\n");
+            
+            foreach( var key in context.Request.Headers.Keys )
+            {
+                await context.Response.WriteAsync($"{key} : {context.Request.Headers[key]}\r\n");
+            }
+        }
+        else if(context.Request.Path.StartsWithSegments("/workers"))
+        {
+            var workers = WorkersRepository.GetWorkers();
+            foreach(var worker in workers)
+            {
+                await context.Response.WriteAsync($"{worker.Id} : {worker.Name} : {worker.Position} : {worker.Salary}\r\n");
+            }
         }
     }
-    else if(context.Request.Path.StartsWithSegments("/worker"))
+    else if (context.Request.Method == "POST")
     {
-        var workers = WorkersRepository.GetWorkers();
-        foreach(var worker in workers)
+        if (context.Request.Path.StartsWithSegments("/workers"))
         {
-            await context.Response.WriteAsync($"{worker.Id} : {worker.Name} : {worker.Position} : {worker.Salary}\r\n");
-        }
+            using var reader = new StreamReader(context.Request.Body);
+            var body = await reader.ReadToEndAsync();
+            Console.WriteLine(body);
+            var newWorker = JsonSerializer.Deserialize<Worker>(body);
+            
+            WorkersRepository.AddWorker(newWorker);
+        }   
     }
 });     // middleware pipeline component
 
-//working with http methods
 
 app.Run();
 
@@ -45,6 +64,15 @@ static class WorkersRepository
     {
         return workers;
     }
+    
+    public static void AddWorker(Worker? worker)
+    {
+        if (worker is not null)
+        {
+            worker.Id = workers.Count + 1;
+            workers.Add(worker);
+        }
+    }   
 }
 
 public class Worker
